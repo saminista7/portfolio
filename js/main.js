7a165182ea2d35c9250629ec94334c4f238f22fe
saminista7 <36105944+saminista7@users.noreply.github.com>
2022-07-14 00:57:48 +0600

    Changed to subfolder

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// Header Scroll
-let nav = document.querySelector(".navbar");
-window.onscroll = function () {
-    if(document.documentElement.scrollTop > 20){
-        nav.classList.add("header-scrolled");
-    }else{
-        nav.classList.remove("header-scrolled");
-    }
-} 
