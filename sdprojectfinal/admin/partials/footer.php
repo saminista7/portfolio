7f76e5184feb3d16bdc4292a22600ed88edaf564
saminista7 <36105944+saminista7@users.noreply.github.com>
2022-09-08 07:31:11 +0600

    Add files via upload

---
+++ sdprojectfinal/admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2020 All rights reserved, AUST. Developed By - <a href="https://www.facebook.com/ahnaf.samin.7"> Ahnaf Samin</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
